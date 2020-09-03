5320b2809d747c61b01976c087fcc60f30329127
priscillaGit1 <kalavakuripriscilla777@gmail.com>
2020-09-04 00:58:08 +0530

    smoke

--- sketch.js
+++ sketch.js
@@ -1,3 +1,19 @@
+var arr1=[1,2,3,4,5];
+console.log(arr1);
+
+var arr2=["name",12,true];
+console.log(arr2);
+
+
+var arr3=[[1,2], [2,3], [4,5]];
+console.log(arr3);
+
+arr3.push("my name");
+console.log(arr3);
+
+arr3.pop();
+console.log(arr3);
+
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
@@ -8,6 +24,7 @@ var box1, pig1,pig3;
 var backgroundImg,platform;
 var bird, slingshot;
 
+var gameState = "on";
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -69,16 +86,19 @@ function draw(){
 }
 
 function mouseDragged(){
-    Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    if (gameState!=="launched"){
+        Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    }
 }
 
 
 function mouseReleased(){
     slingshot.fly();
+    gameState = "launched";
 }
 
 function keyPressed(){
     if(keyCode === 32){
-        slingshot.attach(bird.body);
+       // slingshot.attach(bird.body);
     }
 }
